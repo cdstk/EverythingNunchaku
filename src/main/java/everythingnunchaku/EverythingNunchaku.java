c6dd0c8843073611c875529c2e04bbd35533183a
cdstk <cdstkemail@gmail.com>
2026-02-12 19:14:24 -0600

    Bump 1.0.1

--- src/main/java/everythingnunchaku/EverythingNunchaku.java
+++ src/main/java/everythingnunchaku/EverythingNunchaku.java
@@ -15,7 +15,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = EverythingNunchaku.MODID, version = EverythingNunchaku.VERSION, name = EverythingNunchaku.NAME, dependencies = "required-after:fermiumbooter;required-after:mujmajnkraftsbettersurvival")
 public class EverythingNunchaku {
     public static final String MODID = "everythingnunchaku";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "Everything Nunchaku";
     public static final Logger LOGGER = LogManager.getLogger();
     public static boolean completedLoading = false;
