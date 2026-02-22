9efd24d6b0fc684da69243c8e9f73eebd46bcc60
cdstk <cdstkemail@gmail.com>
2026-02-22 16:06:46 -0600

    Bump 1.0.2

--- src/main/java/everythingnunchaku/EverythingNunchaku.java
+++ src/main/java/everythingnunchaku/EverythingNunchaku.java
@@ -22,7 +22,7 @@ import org.apache.logging.log4j.Logger;
 )
 public class EverythingNunchaku {
     public static final String MODID = "everythingnunchaku";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final String NAME = "Everything Nunchaku";
     public static final Logger LOGGER = LogManager.getLogger();
     public static boolean completedLoading = false;
